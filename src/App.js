df1da80eef1697b9aadf5c02cfe191178355909f
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-25 11:00:01 -0500

    Initial app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Nav from "./Components/Nav";
+import NewTrans from "./Pages/NewTrans";
+import EditTrans from "./Pages/EditTrans";
+import TransDetails from "./Pages/TransDetails";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/new" element={<NewTrans />} />
+          <Route path="/transactions/:id" element={<TransDetails />} />
+          <Route path="/transactions/:id/edit" element={<EditTrans />} />
+        </Routes>
+      </main>
+      
     </div>
   );
 }
