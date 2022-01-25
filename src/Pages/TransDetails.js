86277cd0c62793d556d4fc32f1e37681c624cdc3
Charlie Moran <Carlosamoran18@gmail.com>
2022-01-25 14:30:48 -0500

    trans details 2

--- src/Pages/TransDetails.js
+++ src/Pages/TransDetails.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 import { Link } from "react-router-dom";
-import "./TransactionDetails.css";
+import "./TransDetails.css";
 
 function TransDetails() {
   const params = useParams();
