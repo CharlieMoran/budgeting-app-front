edc0f5769698936b397ef60655513768993db546
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-04 13:09:25 -0500

    72689

--- src/Components/TransTable.js
+++ src/Components/TransTable.js
@@ -25,7 +25,7 @@ class TransTable extends Component {
                   {item.source}
                 </Link>
                 </td>
-                <td>{item.amount}</td>
+                <td>${item.amount.toFixed(2)}</td>
               </tr>
             );
           })}
